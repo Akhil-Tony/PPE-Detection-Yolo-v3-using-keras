ae04f8ad30004d26c41dd195f2fea16f4ed92fae
Akhil Tony Xavier <72643926+Akhil-Tony@users.noreply.github.com>
2022-09-06 11:48:07 +0530

    Update Train_YOLO.py

--- 2_Training/Train_YOLO.py
+++ 2_Training/Train_YOLO.py
@@ -26,112 +26,42 @@ from PIL import Image
 from time import time
 import tensorflow.compat.v1 as tf
 import pickle
-from Train_Utils import get_classes,get_anchors,create_model,create_tiny_model,data_generator_wrapper
+from Train_Utils import get_classes,get_anchors,create_model,data_generator_wrapper
 
 
 keras_path = os.path.join(src_path, "keras_yolo3")
 Data_Folder = os.path.join(get_parent_dir(1), "dataset")
 Image_Folder = os.path.join(Data_Folder, "images")
-VoTT_Folder = os.path.join(Image_Folder)
-YOLO_filename = os.path.join(VoTT_Folder, "data_train.txt")
+data_filename = os.path.join("data_train.txt")
 
 Model_Folder = os.path.join(get_parent_dir(1), 'Data', "Model_Weights")
 YOLO_classname = os.path.join(Model_Folder, "data_classes.txt")
 
-log_dir = Model_Folder
 anchors_path = os.path.join(keras_path, "model_data", "yolo_anchors.txt")
 weights_path = os.path.join(keras_path, "yolo.h5")
 
-FLAGS = None
-
 if __name__ == "__main__":
     
-    parser = argparse.ArgumentParser(argument_default=argparse.SUPPRESS)
-    parser.add_argument("--annotation_file",type=str,default=YOLO_filename)
-    parser.add_argument("--classes_file",type=str,default=YOLO_classname)
-    parser.add_argument("--log_dir",type=str,default=log_dir)
-    parser.add_argument("--anchors_path",type=str,default=anchors_path)
-    parser.add_argument("--weights_path",type=str,default=weights_path)
-    parser.add_argument("--val_split",type=float,default=0.1)
-    parser.add_argument("--is_tiny",default=False,action="store_true")
-    parser.add_argument("--random_seed",type=float,default=None)
-    parser.add_argument("--epochs",type=int,default=51)
-    parser.add_argument("--warnings",default=False,action="store_true")
-
-    FLAGS = parser.parse_args()
-
-    if not FLAGS.warnings:
-        tf.logging.set_verbosity(tf.logging.ERROR)
-        os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
-        warnings.filterwarnings("ignore")
-
-    # Get WandB integration if setup
-    try:
-        import wandb
-        from wandb.integration.keras import WandbCallback  # type: ignore
-
-        wandb.ensure_configured()
-        if wandb.api.api_key is None:
-            _has_wandb = False
-            wandb.termwarn(
-                "W&B installed but not logged in.  Run `wandb login` or set the WANDB_API_KEY env variable."
-            )
-        else:
-            _has_wandb = False if os.getenv("WANDB_DISABLED") else True
-    except (ImportError, AttributeError):
-        _has_wandb = False
-
-    np.random.seed(FLAGS.random_seed)
-
-    log_dir = FLAGS.log_dir
-
-    class_names = get_classes(FLAGS.classes_file)
-    num_classes = len(class_names)
-
-    if FLAGS.is_tiny and FLAGS.weights_path == weights_path:
-        weights_path = os.path.join(os.path.dirname(FLAGS.weights_path), "yolo-tiny.h5")
-    else:
-        weights_path = FLAGS.weights_path
-
-    if FLAGS.is_tiny and FLAGS.anchors_path == anchors_path:
-        anchors_path = os.path.join(
-            os.path.dirname(FLAGS.anchors_path), "yolo-tiny_anchors.txt"
-        )
-    else:
-        anchors_path = FLAGS.anchors_path
-
     anchors = get_anchors(anchors_path)
 
     input_shape = (416, 416)  # multiple of 32, height, width
-    epoch1, epoch2 = FLAGS.epochs, FLAGS.epochs
+    epoch1, epoch2 = 51,51
 
-    is_tiny_version = len(anchors) == 6  # default setting
-    if FLAGS.is_tiny:
-        model = create_tiny_model(input_shape, anchors, num_classes, freeze_body=2, weights_path=weights_path)
-    else:
-        model = create_model(input_shape, anchors, num_classes, freeze_body=2, weights_path=weights_path)
+    model = create_model(input_shape, anchors, num_classes, freeze_body=2, weights_path=weights_path)
 
-    log_dir_time = os.path.join(log_dir, "{}".format(int(time())))
-    logging = TensorBoard(log_dir=log_dir_time)
     checkpoint = ModelCheckpoint(os.path.join(log_dir, "checkpoint.h5"),monitor="val_loss",save_weights_only=True,save_best_only=True,period=5)
     reduce_lr = ReduceLROnPlateau(monitor="val_loss", factor=0.1, patience=3, verbose=1)
     early_stopping = EarlyStopping(monitor="val_loss", min_delta=0, patience=10, verbose=1)
 
     val_split = FLAGS.val_split
-    with open(FLAGS.annotation_file) as f:
+    with open(data_filename) as f:
         lines = f.readlines()
 
     num_val = 30  
     num_train = 250
 
-    # Train with frozen layers first, to get a stable loss.
-    # Adjust num epochs to your dataset. This step is enough to obtain a decent model.
-    frozen_callbacks = [logging, checkpoint]
-
-    if _has_wandb:
-        wandb.init(project="TrainYourOwnYOLO", config=vars(FLAGS), sync_tensorboard=False)
-        wandb_callback = WandbCallback(save_model=False)
-        frozen_callbacks.append(wandb_callback)
+    # Training with frozen layers first, to get a stable loss.
+    frozen_callbacks = [checkpoint]
 
     model.compile(optimizer=Adam(lr=1e-3),loss={"yolo_loss": lambda y_true, y_pred: y_pred})
 
