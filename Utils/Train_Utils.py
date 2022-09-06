f3133c7eab3616a8dc82001c59282a4d88fb4e73
Akhil Tony Xavier <72643926+Akhil-Tony@users.noreply.github.com>
2022-09-06 11:53:02 +0530

    Update Train_Utils.py

--- Utils/Train_Utils.py
+++ Utils/Train_Utils.py
@@ -40,81 +40,27 @@ def create_model(input_shape,anchors,num_classes,load_pretrained=True,freeze_bod
     h, w = input_shape
     num_anchors = len(anchors)
 
-    y_true = [
-        Input(
-            shape=(
-                h // {0: 32, 1: 16, 2: 8}[l],
-                w // {0: 32, 1: 16, 2: 8}[l],
-                num_anchors // 3,
-                num_classes + 5,)
-            ) for l in range(3)]
+    y_true = [Input(shape=(h // {0: 32, 1: 16, 2: 8}[l],w // {0: 32, 1: 16, 2: 8}[l],num_anchors // 3,num_classes + 5,)) for l in range(3)]
 
     model_body = yolo_body(image_input, num_anchors // 3, num_classes)
     
     print("Create YOLOv3 model with {} anchors and {} classes.".format(num_anchors, num_classes))
 
-    if load_pretrained:
-        model_body.load_weights(weights_path, by_name=True, skip_mismatch=True)
-        
-        print("Load weights {}.".format(weights_path))
-        
-        if freeze_body in [1, 2]:              # Freeze darknet53 body or freeze all but 3 output layers.
-            num = (185, len(model_body.layers) - 3)[freeze_body - 1]
-            for i in range(num):
-                model_body.layers[i].trainable = False
-            print("Freeze the first {} layers of total {} layers.".format(num, len(model_body.layers)))
+    model_body.load_weights(weights_path, by_name=True, skip_mismatch=True)
+    if freeze_body in [1, 2]:              # Ffreeze all except 3 output layers.
+        num = (185, len(model_body.layers) - 3)[freeze_body - 1]
+        for i in range(num):
+            model_body.layers[i].trainable = False
 
     model_loss = Lambda(
         yolo_loss,
         output_shape=(1,),
         name="yolo_loss",
-        arguments={
-            "anchors": anchors,
-            "num_classes": num_classes,
-            "ignore_thresh": 0.5,
-        },
-    )([*model_body.output, *y_true])
+        arguments={"anchors": anchors,"num_classes": num_classes,"ignore_thresh": 0.5,},)([*model_body.output, *y_true])
     model = Model([model_body.input, *y_true], model_loss)
 
     return model
 
-
-def create_tiny_model(input_shape,anchors,num_classes,load_pretrained=True,freeze_body=2,
-    weights_path="keras_yolo3/model_data/tiny_yolo_weights.h5",):
-
-    K.clear_session()  # get a new session
-    image_input = Input(shape=(None, None, 3))
-    h, w = input_shape
-    num_anchors = len(anchors)
-
-    y_true = [ Input(shape=(
-                h // {0: 32, 1: 16}[l],
-                w // {0: 32, 1: 16}[l],
-                num_anchors // 2,
-                num_classes + 5,
-            ) ) for l in range(2)]
-
-    model_body = tiny_yolo_body(image_input, num_anchors // 2, num_classes)
-    print("Create Tiny YOLOv3 model with {} anchors and {} classes.".format(num_anchors, num_classes))
-
-    if load_pretrained:
-        model_body.load_weights(weights_path, by_name=True, skip_mismatch=True)
-        print("Load weights {}.".format(weights_path))
-        if freeze_body in [1, 2]:
-            # Freeze the darknet body or freeze all but 2 output layers.
-            num = (20, len(model_body.layers) - 2)[freeze_body - 1]
-            for i in range(num):
-                model_body.layers[i].trainable = False
-            
-            print("Freeze the first {} layers of total {} layers.".format(num, len(model_body.layers)))
-
-    model_loss = Lambda(yolo_loss,output_shape=(1,),
-        name="yolo_loss",arguments={"anchors": anchors,"num_classes": num_classes,
-        "ignore_thresh": 0.7,},)([*model_body.output, *y_true])
-
-    model = Model([model_body.input, *y_true], model_loss)
-
-    return model
 
 
 def data_generator(annotation_lines, batch_size, input_shape, anchors, num_classes):
@@ -137,13 +83,8 @@ def data_generator(annotation_lines, batch_size, input_shape, anchors, num_class
         yield [image_data, *y_true], np.zeros(batch_size)
 
 
-def data_generator_wrapper(
-    annotation_lines, batch_size, input_shape, anchors, num_classes
-):
+def data_generator_wrapper(annotation_lines, batch_size, input_shape, anchors, num_classes):
     n = len(annotation_lines)
     if n == 0 or batch_size <= 0:
         return None
-    return data_generator(
-        annotation_lines, batch_size, input_shape, anchors, num_classes
-    )
-
+    return data_generator(annotation_lines, batch_size, input_shape, anchors, num_classes)
